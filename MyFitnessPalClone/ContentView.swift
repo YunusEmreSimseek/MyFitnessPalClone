f7125f9a01b234d174b1b1f4896d8ea783aef638
YunusEmreSimseek <yemresimseek@gmail.com>
2024-08-16 11:32:33 +0300

    Initial Commit

---
+++ MyFitnessPalClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyFitnessPalClone
+//
+//  Created by Emre Simsek on 16.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyFitnessPalClone/MyFitnessPalCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFitnessPalCloneApp.swift
+//  MyFitnessPalClone
+//
+//  Created by Emre Simsek on 16.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyFitnessPalCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
